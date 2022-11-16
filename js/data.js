63cc6c001f12cb9b54d593a85eaf9aacd1ac425c
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-11-16 22:03:11 +0000

    completed issue 2 features, added column graph and take care of parsing and storing data to data model and set up a local storage to save entries and keep track of entryId

--- js/data.js
+++ js/data.js
@@ -1,2 +1,19 @@
 /* exported data */
-var data = [];
+var data = {
+  entryId: 1,
+  footprints: [],
+  editing: null
+};
+
+var previousFootprintEntries = localStorage.getItem('javascript-local-storage');
+if (previousFootprintEntries !== null) {
+  data = JSON.parse(previousFootprintEntries);
+}
+
+window.addEventListener('beforeunload', beforeUnloadHandle);
+
+function beforeUnloadHandle(event) {
+  data.editing = null;
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -93,7 +93,10 @@ function getResult(answers) {
   xhr.setRequestHeader('Authorization', bearer);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    data.push(xhr.response);
+    var newFootprint = xhr.response;
+    var parsedFootprint = parseAPIData(newFootprint);
+    data.footprints.push(parsedFootprint);
+    updateResultPage(parsedFootprint);
     viewSwap('result');
   });
   xhr.send(formData);
@@ -102,8 +105,8 @@ function getResult(answers) {
 function parseAnswer(answers) {
   var vehicles = {
     sedan: 'passenger_vehicle-vehicle_type_car-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
-    truck: 'passenger_vehicle-vehicle_type_pickup_trucks_vans_suvs-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
-    suv: 'passenger_vehicle-vehicle_type_pickup_trucks_vans_suvs-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
+    truck: 'commercial_vehicle-vehicle_type_truck_medium_or_heavy-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
+    suv: 'commercial_vehicle-vehicle_type_truck_light-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
     none: 'passenger_vehicle-vehicle_type_bus-fuel_source_na-distance_na-engine_size_na',
     motorcycle: 'passenger_vehicle-vehicle_type_motorcycle-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na'
   };
@@ -146,4 +149,66 @@ function parseAnswer(answers) {
     }
   ];
   return JSON.stringify(climatiqObj);
+}
+
+// Parsing data
+// Access CO2e -> Object[index].constituent_gases.co2e_total
+// Access CO2  -> Object[index].constituent_gases.co2
+// Access CH4  -> Object[index].constituent_gases.ch4
+// Access N2O  -> Object[index].constituent_gases.n2o
+
+function parseAPIData(dataAPI) {
+  var newFootprintObject = {
+    date: null,
+    entryId: null,
+    data: null,
+    total: {
+      co2e: 0,
+      co2: 0,
+      ch4: 0,
+      n2o: 0
+    }
+  };
+  newFootprintObject.entryId = data.entryId;
+  data.entryId++;
+  var date = new Date();
+  newFootprintObject.date = String(date.getMonth()) + '/' + String(date.getDate()) + '/' + String(date.getFullYear());
+  newFootprintObject.data = dataAPI;
+  for (var i = 0; i < dataAPI.results.length; i++) {
+    if (dataAPI.results[i].constituent_gases.co2e_total) {
+      newFootprintObject.total.co2e += dataAPI.results[i].constituent_gases.co2e_total;
+    }
+    if (dataAPI.results[i].constituent_gases.co2) {
+      newFootprintObject.total.co2 += dataAPI.results[i].constituent_gases.co2;
+    }
+    if (dataAPI.results[i].constituent_gases.ch4) {
+      newFootprintObject.total.ch4 += dataAPI.results[i].constituent_gases.ch4;
+    }
+    if (dataAPI.results[i].constituent_gases.n2o) {
+      newFootprintObject.total.n2o += dataAPI.results[i].constituent_gases.n2o;
+    }
+  }
+  return newFootprintObject;
+}
+
+function updateResultPage(obj) {
+  var $resultGraphCO2e = document.querySelector('#resultGraphCO2e');
+  var $resultGraphCO2 = document.querySelector('#resultGraphCO2e');
+  var $resultGraphCH4 = document.querySelector('#resultGraphCH4');
+  var $resultGraphN2O = document.querySelector('#resultGraphN2O');
+  var $resultTextCO2e = document.querySelector('#CO2eResultText');
+  var $resultTextCO2 = document.querySelector('#CO2ResultText');
+  var $resultTextCH4 = document.querySelector('#CH4ResultText');
+  var $resultTextN2O = document.querySelector('#N2OResultText');
+  var $resultDate = document.querySelector('#resultDate');
+
+  $resultGraphCH4.textContent = Number.parseFloat(obj.total.ch4.toPrecision(3));
+  $resultGraphCO2.textContent = Number.parseFloat(obj.total.co2.toPrecision(3));
+  $resultGraphCO2e.textContent = Number.parseFloat(obj.total.co2e.toPrecision(3));
+  $resultGraphN2O.textContent = Number.parseFloat(obj.total.n2o.toPrecision(3));
+  $resultTextCH4.textContent = Number.parseFloat(obj.total.ch4.toPrecision(3));
+  $resultTextCO2.textContent = Number.parseFloat(obj.total.co2.toPrecision(3));
+  $resultTextCO2e.textContent = Number.parseFloat(obj.total.co2e.toPrecision(3));
+  $resultTextN2O.textContent = Number.parseFloat(obj.total.n2o.toPrecision(3));
+  $resultDate.textContent = obj.date;
 }
